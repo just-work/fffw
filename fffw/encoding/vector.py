6520ede5224ea0586cb3e9a6b31819dc530b5995
tumb1er <zimbler@gmail.com>
2020-06-05 17:36:57 +0300

    #24 apply same filter with mask

--- fffw/encoding/vector.py
+++ fffw/encoding/vector.py
@@ -96,7 +96,7 @@ class Vector:
 
 
 class Cursor:
-    def __init__(self, vector: Vector, 
+    def __init__(self, vector: Vector,
                  *streams: Union[inputs.Stream, filters.Filter]) -> None:
         self.vector = vector
         self.streams = streams
@@ -107,31 +107,35 @@ class Cursor:
             return NotImplemented
         return self.connect(other)
 
-    def __apply(self, vector: Union[List[filters.Filter], List[outputs.Codec]],
-                mask: List[bool] = None) -> "Cursor":
+    def __apply(self, vector: Union[List[Optional[filters.Filter]],
+                                    List[outputs.Codec]]) -> "Cursor":
         """ Applies each filter in list to it's corresponding stream.
 
         :param vector: list of filters or list of codecs
-        :param mask: flag for skipping a filter in list
         :returns: new streams vector.
         """
-        if mask and not all(mask):
-            raise NotImplementedError()
         sources = dict()
         destinations = dict()
+        indices = defaultdict(list)
         src_to_dst = defaultdict(set)
-        for src, dst in zip(self.streams, vector):
+        for src, dst, i in zip(self.streams, vector, range(len(vector))):
             sources[id(src)] = src
             destinations[id(dst)] = dst
             # Splitting single source to connect to multiple outputs
             src_to_dst[id(src)].add(id(dst))
+            indices[id(src), id(dst)].append(i)
+
         for src_id, dst_ids in src_to_dst.items():
             src = sources[src_id]
             split = filters.Split(self.kind, output_count=len(dst_ids))
             src.connect_dest(split)
             for dst_id in dst_ids:
                 dst = destinations[dst_id]
-                split.connect_dest(dst)
+                if dst is not None:
+                    split.connect_dest(dst)
+                else:
+                    for idx in indices[id(src), id(dst)]:
+                        vector[idx] = split
         return Cursor(self.vector, *vector)
 
     def connect(self,
@@ -155,6 +159,7 @@ class Cursor:
         >>> cursor.connect(overlay, mask=[True, False, True])
 
         """
+        vector: List[Optional[filters.Filter]]
         if isinstance(other, filters.Filter):
             if params is not None:
                 raise ValueError("params already passed to filter instance")
@@ -174,10 +179,14 @@ class Cursor:
 
         if mask is None:
             mask = [True] * len(self.streams)
+
         if len(mask) != len(self.streams):
             raise ValueError("mask vector doesn't match streams vector")
-
-        return self.__apply(vector, mask)
+        else:
+            for i, enabled in enumerate(mask):
+                if not enabled:
+                    vector[i] = None
+        return self.__apply(vector)
 
     def finalize(self) -> None:
         """ Connect streams to codecs."""
--- tests/test_vector.py
+++ tests/test_vector.py
@@ -39,6 +39,7 @@ class VectorTestCase(TestCase):
         self.assertEqual(expected, self.vector.ffmpeg.get_args())
 
     def test_same_filter_for_all_streams(self):
+        """ Single filter can be applied to each stream in vector."""
         cursor = self.vector.audio.connect(Volume(30))
         cursor.finalize()
         expected = ensure_binary([
@@ -48,6 +49,60 @@ class VectorTestCase(TestCase):
             '-filter_complex',
             '[0:a]volume=30.00[a:volume0];'
             '[a:volume0]asplit[aout0][aout1]',
+            '-map', '0:v', '-c:v', 'libx264',
+            '-map', '[aout0]', '-c:a', 'aac',
+            'output1.mp4',
+            '-map', '0:v', '-c:v', 'libx265',
+            '-map', '[aout1]', '-c:a', 'libfdk_aac',
+            'output2.mp5'])
+        self.assertEqual(expected, self.vector.ffmpeg.get_args())
+
+    def test_same_filter_with_mask(self):
+        """ Applying filter works with mask."""
+        cursor = self.vector.audio.connect(Volume(30), mask=[False, True])
+        cursor.finalize()
+        expected = ensure_binary([
+            'ffmpeg',
+            '-i',
+            'input.mp4',
+            '-filter_complex',
+            '[0:a]asplit[a:asplit0][aout0];'
+            '[a:asplit0]volume=30.00[aout1]',
+            '-map', '0:v', '-c:v', 'libx264',
+            '-map', '[aout0]', '-c:a', 'aac',
+            'output1.mp4',
+            '-map', '0:v', '-c:v', 'libx265',
+            '-map', '[aout1]', '-c:a', 'libfdk_aac',
+            'output2.mp5'])
+        self.assertEqual(expected, self.vector.ffmpeg.get_args())
+
+    def test_same_filter_with_another_mask(self):
+        cursor = self.vector.audio.connect(Volume(30), mask=[True, False])
+        cursor.finalize()
+        expected = ensure_binary([
+            'ffmpeg',
+            '-i',
+            'input.mp4',
+            '-filter_complex',
+            '[0:a]asplit[a:asplit0][aout0];'
+            '[a:asplit0]volume=30.00[aout1]',
+            '-map', '0:v', '-c:v', 'libx264',
+            '-map', '[aout1]', '-c:a', 'aac',
+            'output1.mp4',
+            '-map', '0:v', '-c:v', 'libx265',
+            '-map', '[aout0]', '-c:a', 'libfdk_aac',
+            'output2.mp5'])
+        self.assertEqual(expected, self.vector.ffmpeg.get_args())
+
+    def test_multiple_disabled_filters(self):
+        cursor = self.vector.audio.connect(Volume(30), mask=[False, False])
+        cursor.finalize()
+        expected = ensure_binary([
+            'ffmpeg',
+            '-i',
+            'input.mp4',
+            '-filter_complex',
+            '[0:a]asplit[aout0][aout1]',
             '-map', '0:v', '-c:v', 'libx264',
             '-map', '[aout0]', '-c:a', 'aac',
             'output1.mp4',
