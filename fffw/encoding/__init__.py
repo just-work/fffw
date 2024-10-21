bc7b6a80cc8733777ddadda0a14e8da714a74244
tumb1er <zimbler@gmail.com>
2024-10-21 11:55:39 +0300

    FFProbe wrapper

--- fffw/encoding/__init__.py
+++ fffw/encoding/__init__.py
@@ -1,5 +1,6 @@
 from .codecs import *
 from .ffmpeg import *
+from .ffprobe import *
 from .filters import *
 from .inputs import *
 from .outputs import *
---
+++ fffw/encoding/ffprobe.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+from fffw.wrapper import BaseWrapper, param
+
+__all__ = ['FFProbe']
+
+
+@dataclass
+class FFProbe(BaseWrapper):
+    """
+    ffprobe command line basic wrapper.
+
+    >>> ff = FFProbe(
+    ...     '/tmp/input.mp4',
+    ...     show_streams=True,
+    ...     show_format=True,
+    ...     output_format='json')
+    >>> ff.get_cmd()
+    'ffprobe -show_streams -show_format -of json -i /tmp/input.mp4'
+    >>>
+    """
+    command = 'ffprobe'
+    input: str = param(name='i')
+    """ Input media uri/path."""
+    loglevel: str = param()
+    """ Loglevel: i.e. `level+info`."""
+    show_streams: bool = param(default=False)
+    show_format: bool = param(default=False)
+    output_format: str = param(name='of')
